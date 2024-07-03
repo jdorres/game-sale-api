75d585729eddac338f879f402d7d06f88ac56f5d
Julio <juliodorres@gmail.com>
2024-07-03 16:29:13 -0300

    added user crud

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserStoreRequest;
+use App\Http\Requests\UserUpdateRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Http;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = User::all();
+        return response()->json(UserResource::collection($users), Response::HTTP_CREATED);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(UserStoreRequest $request)
+    {
+        try{
+            $data = $request->validated();
+            $user = User::create($data);
+            response()->json(new UserResource($user), Response::HTTP_CREATED);
+        }catch(Exception $e){
+            //TODO
+            dd($e->getMessage());
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(int $id){
+        $user = User::findOrFail($id);
+        return response()->json(new UserResource($user), Response::HTTP_OK);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UserUpdateRequest $request, string $id)
+    {
+        try{
+            $data = $request->all();
+            $user = User::findOrFail($id);
+            $user->update($data);
+            return response()->json($user, Response::HTTP_OK);
+        }catch(Exception $e){
+            //TODO
+            dd($e->getMessage());
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        User::destroy($id);
+        return response()->json([], Response::HTTP_OK);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\PurchaseController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::get('/users', [UserController::class, 'index']);
+Route::post('/users', [UserController::class, 'store']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+Route::post('/users/{id}', [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
