b83c51e4485368900626ecb2bf1cbf9ff0ed82ac
Julio <juliodorres@gmail.com>
2024-07-18 15:12:27 -0300

    adicionados seeders para gateway e paymentMethod

---
+++ database/seeders/GatewaySeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Gateway;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class GatewaySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Gateway::create([
+            'name' => "Asaas",
+            'code' => "asaas",
+            'default' => true
+        ]);
+    }
+}
